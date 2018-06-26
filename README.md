17e71ba65438bdf3e8655ae55c6c9823a5610b0c
Taylor Hafley <taylor.hafley.uga@gmail.com>
2018-06-26 14:53:28 -0400

    vignette

--- README.md
+++ README.md
@@ -3,27 +3,26 @@
 This project establishes sub/urban boundaries. These boundaries offer one approach to visualize and analyze sub/urbanization in US metropolitan areas from 1940 to 2010. We use the Hammer Method to backcast estimates of housing units at the block group (or census tract) during each decade. The Hammer method uses historic county-level Census data (nhgis.org), and current county-level and block group (or census tracts) ACS data to estimate the number of housing units. After generating the estimate for each sub-geography in each time period, we normalize the data by dividing the number of units by the sub-geography area (square miles). The output, then, is the number of housing units per square mile within a given block group.
 
 ## Getting started
+
+### Files 
 Aside from the area computation of the sub-geographies (completed in ArcGIS), all work is completed in RStudio. Most necessary data is available in the [data](data) folder. All scripts are located in the [scripts](scripts) folder.
 
 Scott - The 1990 and 2000 county-level data is located in our shared dropbox. It is too large to be loaded to GitHub.
 
-## Files 
-
-
 #### Scripts
 There are three primary files:
-1. [h_units](scripts/h_units.R): this file reads in, cleans, and combines separate county-level housing units from 1940-2010 into a single file [h_units_national.csv](data/h_units_national.csv)
+1. [h_units.R](scripts/h_units.R): this file reads in, cleans, and combines separate county-level housing units from 1940-2010 into a single file [h_units_national.csv](data/h_units_national.csv)
 
-2. [bg_hammer](scripts/bg_hammer.R) : This script reads in all data necessary to calculate number of housing units per square mile by block group, nationwide, using the Hammer Method.  Script reads in 'Year Structure Built' block group data from the 2011-15 ACS, block group area in square miles, and reads in h_units to join. 
+2. [bg_hammer.R](scripts/bg_hammer.R) : This script reads in all data necessary to calculate number of housing units per square mile by block group, nationwide, using the Hammer Method.  Script reads in 'Year Structure Built' block group data from the 2011-15 ACS, block group area in square miles, and reads in h_units to join. 
 
 3. ct_hammer : This script reads in all data necessary to calculate number of housing units per square mile by census tract, nationwide, using the Hammer Method.  Script reads in 'Year Structure Built' census tract data from the 2011-15 ACS, census tract area in square miles, and reads in h_units to join. 
 
-Finally, [UShammerMethodBG](scripts/UShammerMethodBG.R) is a complete script. It includes step-by-step calculations of the number of housing units per square mile in each block group, nationwide. 
+Finally, [UShammerMethodBG](scripts/UShammerMethodBG.R) is a complete script. Essentially, it is a combination of h_units.R and bg_hammer.R that I did my initial analysis in. I separted this into two scripts to hopefully make things a bit easier to follower. It includes step-by-step calculations of the number of housing units per square mile in each block group, nationwide. You shouldn't need this, but if you encounter problems in scripts #1 or #2 listed above, this is a place to look.
 
 This script reads in all data necessary to calculate adjusted housing units using the Hammer Method. All data is located in the 'data' folder. This script reads in 'Year Structure Built' block group data from the 2011-15 ACS block group data; a BG .txt file where block group areas were calculated in ArcGIS after reprojecting NHGIS data; and county-level historical counts of housing units (1950 and 1960 are from mhauer; all other decades were downloaded from NHGIS). 
 
-Similarly, [UShammerMethodCT](scripts/UShammerCT.R) is modeled after this file, yields the number of housing units per square mile at the census tract level, nationwide. The steps are the same, but the results are aggregated to a different sub-geography. In this script, I do not read in each separate county-level historical counts of housing units, I just read in the file generated in [UShammerMethodBG](scripts/UShammerMethodBG.R). This single, complete historic housing unit file is located [here](csv/h_units_national.csv)
-
+Similarly, [UShammerMethodCT](scripts/UShammerCT.R) is modeled after its BG counterpart, instead yielding the number of housing units per square mile at the census tract level, nationwide. The steps are the same, but the results are aggregated to a different sub-geography. In this script, I do not read in each separate county-level historical counts of housing units, I just read in the file generated in [UShammerMethodBG](scripts/UShammerMethodBG.R). 
 
 
 ### additional notes
+I have some animations that I've been working on that are not on the GitHub. I will load them in very soon.
--- scripts/bg_hammer.R
+++ scripts/bg_hammer.R
@@ -14,7 +14,7 @@ library(readxl)
 
 # read in county-level number of housing units, 1940-2010
 # calculated in h_units.R
-h3 <- read_csv("csv/h_units_national.csv")
+h_units_national <- read_csv("csv/h_units_national.csv")
 
 # read in housing units by 'year structure built' from 2011-15 ACS, downloaded from NHGIS
 # metadata: data/nhgis0051_csv/nhgis0051_ds215_20155_2015_blck_grp_codebook.txt
@@ -29,7 +29,7 @@ USbg <- USbg %>%
 
 # read block group area
 # area unit: square miles of each block group.
-# area calculated in ArcGIS; area calculated in equal area projection
+# area calculated in ArcGIS; re-projected and calculated using equal area projection
 bg_area <- read_csv("data/areas.txt")
 
 # remove unecessary columns
@@ -37,14 +37,12 @@ bg_area <- bg_area %>%
   select(-c(7:9))
 
 # join area to 'Year Structure Built' tibble
-USbg2 <- USbg %>%
+USbg <- USbg %>%
   left_join(x = USbg, y = bg_area, by = "GISJOIN") %>%
   rename(geoid = GEOID)
 
-
-
 # join county housing units to block group tibble
-US_bg3 <- left_join(USbg2,h3, by = 'COUNTYJ')
+US_bg2 <- left_join(USbg,h_units_national, by = 'COUNTYJ')
 
 
 # jXX = total number of housing units in county during time t, based on year structure built in ACS
@@ -52,20 +50,26 @@ US_bg3 <- left_join(USbg2,h3, by = 'COUNTYJ')
 # adjXX = adjusted number of housing units in block group, hammer method output
 # additional reference: https://www.nature.com/articles/nclimate2961#methods
 
-US_bg3 <- US_bg3 %>%
+US_bg2 <- US_bg2 %>%
   group_by(COUNTYJ) %>%
   mutate(
+    j50 = sum(ADQSE010, ADQSE011),
+    j60 = sum(ADQSE009, ADQSE010, ADQSE011),
     j70 = sum(ADQSE008, ADQSE009, ADQSE010, ADQSE011),
     j80 = sum(ADQSE007,ADQSE008, ADQSE009, ADQSE010, ADQSE011),
     j90 = sum(ADQSE006,ADQSE007,ADQSE008, ADQSE009, ADQSE010, ADQSE011),
     j00 = sum(ADQSE005,ADQSE006,ADQSE007,ADQSE008, ADQSE009, ADQSE010, ADQSE011),
     j10 = sum(ADQSE004,ADQSE005,ADQSE006,ADQSE007,ADQSE008, ADQSE009, ADQSE010, ADQSE011),
+    i50 = (ADQSE010 + ADQSE011),
+    i60 = (ADQSE009 + ADQSE010 + ADQSE011),
     i70 = (ADQSE008 + ADQSE009 + ADQSE010 + ADQSE011),
     i80 = (ADQSE007 + ADQSE008 + ADQSE009 + ADQSE010 + ADQSE011),
     i90 = (ADQSE006 + ADQSE007 + ADQSE008 + ADQSE009 + ADQSE010 + ADQSE011),
     i00 = (ADQSE005 + ADQSE006 + ADQSE007 + ADQSE008 + ADQSE009 + ADQSE010 + ADQSE011),
     i10 = (ADQSE004 + ADQSE005 + ADQSE006 + ADQSE007 + ADQSE008 + ADQSE009 + ADQSE010 + ADQSE011),
     adj40 = (hu40/sum(ADQSE011)*ADQSE011),
+    adj50 = hu50/j50 * i50,
+    adj60 = hu60/j60 * i60,
     adj70 = hu70/j70 * i70,
     adj80 = hu80/j80 * i80,
     adj90 = hu90/j90 * i90,
@@ -77,10 +81,12 @@ US_bg3 <- US_bg3 %>%
   )
 
 # huXX_sqmi = housing units/square mile
-US_bg3 <- US_bg3 %>%
+US_bg2 <- US_bg2 %>%
   group_by(COUNTYJ) %>%
   mutate(
     hu40_sqmi = adj40/area,
+    hu50_sqmi = adj50/area,
+    hu60_sqmi = adj60/area,
     hu70_sqmi = adj70/area,
     hu80_sqmi = adj80/area,
     hu90_sqmi = adj90/area,
@@ -88,4 +94,79 @@ US_bg3 <- US_bg3 %>%
     hu10_sqmi = adj10/area
   )
 
-# write_csv(US_bg3, "csv/blk_grp_complete.csv")
+# write_csv(US_bg2, "csv/blk_grp_complete.csv")
+
+
+
+
+
+
+
+
+
+
+######################################################################
+# visualizations
+#******************
+
+#labels <- c(G1300670 = 'Cobb', G1301210 = 'Fulton', G1300890 = 'DeKalb',
+#            G1301350 = 'Gwinnett', G1302190 = 'Oconee', G1300590 = 'Clarke')
+
+US_bg2 %>% 
+  filter(STATEA == 13 & COUNTYJ %in% c('G1300670','G1301210','G1300890','G1301350','G1302190',
+                                       'G1300590') & hu90_sqmi >10 & hu90_sqmi < 800) %>%
+  ggplot((mapping = aes(x = hu90_sqmi)))+
+  geom_histogram(bins = 10) +
+  labs(title = "1990 Housing Units", x = "hu/sq mi") +
+  facet_wrap('COUNTYJ', labeller = labeller(COUNTYJ = labels))
+
+
+US_bg2 %>% 
+  filter(STATEA == 13 & COUNTYJ %in% c('G1300670','G1301210','G1300890','G1301350','G1302190',
+                                       'G1300590')) %>%
+  ggplot(mapping = aes(x=COUNTYJ, y = hu40_sqmi)) +
+  geom_boxplot()
+
+# write_csv(test1c,"Dropbox/hafley/urbanization/test1c.csv") #If you want to save as a file
+
+
+
+
+
+
+# skip for now, commented out
+US_bg2$urb40 <- ''
+US_bg2[,'urb40'] <- sapply(US_bg2[,'hu40_sqmi'],function(x)ifelse(x > 200,1,0))
+US_bg2$urb40 <- as.factor(US_bg2$urb40)
+
+# set up urban classification
+US_bg2$urb50 <- ''
+US_bg2$urb60 <- ''
+US_bg2$urb70 <- ''
+US_bg2$urb80 <- ''
+US_bg2$urb90 <- ''
+US_bg2$urb00 <- ''
+US_bg2$urb10 <- ''
+
+# determine if area is urban. If huXX_sqmi > 200 or if previous decade is
+# classified as urban, then bg is urban, according to this approach
+US_bg2[,'urb50'] <- ifelse((US_bg2$hu50_sqmi > 200 | US_bg2$urb40 == 1),1,0)
+US_bg2[,'urb60'] <- ifelse((US_bg2$hu60_sqmi > 200 | US_bg2$urb50 == 1),1,0)
+US_bg2[,'urb70'] <- ifelse((US_bg2$hu70_sqmi > 200 | US_bg2$urb40 == 1),1,0)
+US_bg2[,'urb80'] <- ifelse((US_bg2$hu80_sqmi > 200 | US_bg2$urb70 == 1),1,0)
+US_bg2[,'urb90'] <- ifelse((US_bg2$hu90_sqmi > 200 | US_bg2$urb80 == 1),1,0)
+US_bg2[,'urb00'] <- ifelse((US_bg2$hu00_sqmi > 200 | US_bg2$urb90 == 1),1,0)
+US_bg2[,'urb10'] <- ifelse((US_bg2$hu10_sqmi > 200 | US_bg2$urb00 == 1),1,0)
+
+
+# convert to factors
+US_bg2$urb40 <- as.factor(US_bg2$urb40)
+US_bg2$urb50 <- as.factor(US_bg2$urb50)
+US_bg2$urb60 <- as.factor(US_bg2$urb60)
+US_bg2$urb70 <- as.factor(US_bg2$urb70)
+US_bg2$urb80 <- as.factor(US_bg2$urb80)
+US_bg2$urb90 <- as.factor(US_bg2$urb90)
+US_bg2$urb00 <- as.factor(US_bg2$urb00)
+US_bg2$urb10 <- as.factor(US_bg2$urb10)
+
+US_bg2
